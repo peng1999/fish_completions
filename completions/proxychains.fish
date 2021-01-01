4dad0fc519aa6cbff70350b328b47e79a63b21d4
Peng Guanwen <pg999w@outlook.com>
2021-01-01 16:17:19 +0800

    Add proxychains completion

---
+++ completions/proxychains.fish
@@ -0,0 +1,3 @@
+complete -c proxychains -s q -d 'Makes proxychains quiet'
+complete -c proxychains -s f -r -d 'Allows one to manually specify a configfile to use'
+complete -c proxychains -w command
