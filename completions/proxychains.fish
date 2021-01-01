7ee1c52da208919dd4f397574beb27f73c06a67d
Peng Guanwen <pg999w@outlook.com>
2021-01-01 16:22:38 +0800

    Update proxychains

--- completions/proxychains.fish
+++ completions/proxychains.fish
@@ -1,3 +1,4 @@
 complete -c proxychains -s q -d 'Makes proxychains quiet'
 complete -c proxychains -s f -r -d 'Allows one to manually specify a configfile to use'
-complete -c proxychains -w command
+complete -c proxychains -l help -d 'Display help and exit'
+complete -c proxychains -xa "(__fish_complete_subcommand)"
